c13ad0e9e39ffa6f38706261c8b8e46502b8831c
xcf13363175 <13363175@qq.com>
2022-11-16 12:58:20 +0800

    Update ipq807x-diy-part2.sh

--- ipq807x-diy-part2.sh
+++ ipq807x-diy-part2.sh
@@ -30,7 +30,7 @@ sed -i 's/IMG_PREFIX:=$(VERSION_DIST_SANITIZED)/IMG_PREFIX:=$(shell TZ=UTC-8 dat
 
 #5.更换lede源码中自带argon主题
 rm -rf feeds/luci/themes/luci-theme-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon 
-
+rm -rf feeds/small8/netmaker
 #6.添加自动挂载磁盘脚本
 #mkdir -p files/etc/hotplug.d/block && wget -O files/etc/hotplug.d/block/30-usbmount https://raw.githubusercontent.com/ficheny/P3TERX_Actions-OpenWrt/main/files/etc/hotplug.d/block/30-usbmount && chmod 755 files/etc/hotplug.d/block/30-usbmount
 
@@ -86,6 +86,5 @@ git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-a
 pushd package
 git clone -b v3.3 https://github.com/Mmx233/BitSrunLoginGo_Openwrt
 git clone https://github.com/sbwml/luci-app-mosdns
-git clone https://github.com/sbwml/v2ray-geodata
 svn co https://github.com/xcf13363175/netmaker-openwrt/trunk/netmaker
 popd
