0ade62aee57b4f047f6dddb6b4801f423c547984
xcf13363175 <13363175@qq.com>
2022-11-16 13:17:51 +0800

    Update ipq807x-diy-part2.sh

--- ipq807x-diy-part2.sh
+++ ipq807x-diy-part2.sh
@@ -66,7 +66,7 @@ sed -i '/exit 0/i sed -i "s/services/system/g" /usr/lib/lua/luci/controller/cpuf
 #添加额外非必须软件包
 #git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
 #git clone https://github.com/garypang13/luci-app-bypass package/luci-app-bypass
-git clone https://github.com/vernesong/OpenClash.git package/OpenClash
+
 #git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
 
 #git clone https://github.com/sirpdboy/luci-app-netdata.git package/luci-app-netdata
@@ -80,11 +80,9 @@ git clone https://github.com/vernesong/OpenClash.git package/OpenClash
 
 #git clone https://github.com/riverscn/openwrt-iptvhelper.git package/openwrt-iptvhelper
 #添加smartdns
-git clone https://github.com/pymumu/openwrt-smartdns package/smartdns
-git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
 #git clone -b luci https://github.com/pexcn/openwrt-chinadns-ng.git package/luci-app-chinadns-ng
 pushd package
-git clone -b v3.3 https://github.com/Mmx233/BitSrunLoginGo_Openwrt
-git clone https://github.com/sbwml/luci-app-mosdns
+#git clone -b v3.3 https://github.com/Mmx233/BitSrunLoginGo_Openwrt
+#git clone https://github.com/sbwml/luci-app-mosdns
 svn co https://github.com/xcf13363175/netmaker-openwrt/trunk/netmaker
 popd
